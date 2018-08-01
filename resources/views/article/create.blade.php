0c10fa6651d8977841a98d96e1df18b235013a11
tianxin <844577216@qq.com>
2018-08-01 10:55:35 +0800

    markdown拓展服务提供者

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,11 +1,26 @@
 <?php
 use Encore\Admin\Widgets\Form;
+use Encore\Admin\Widgets\Box;
 
 
 
-$form = new Form();
-$form->text('name', '输入框');
-echo $form->render();
+//$form = new Form();
+//$form->text('name', '输入框');
+//echo $form->render();
+
+$box = new Box('Box标题', 'Box内容');
+
+$box->removable();
+
+$box->collapsable();
+
+$box->style('info');
+
+$box->solid();
+
+echo $box;
+
+?>
 
 
 
