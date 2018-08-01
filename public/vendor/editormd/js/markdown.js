c38ee7ac9f31762753a2ddc978f3411fda445dc6
田鑫 <844577216@qq.com>
2018-08-01 21:13:13 +0800

    markdown

--- public/vendor/editormd/js/markdown.js
+++ public/vendor/editormd/js/markdown.js
@@ -6,6 +6,6 @@ $(function () {
     };
     Editormd = editormd({
         id: "editormd_id",
-        path:window.location.protocol+'//'+window.location.host+'/vendor/lib/'
+        path:window.location.protocol+'//'+window.location.host+'/vendor/editormd/lib/'
     });
 });
