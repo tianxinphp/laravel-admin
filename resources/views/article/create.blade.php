23b08b4105bcdc5b08e498d804e4e766152f4249
tianxin <844577216@qq.com>
2018-08-01 11:41:21 +0800

    markdown拓展服务提供者

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,84 +1,11 @@
 <?php
 use Encore\Admin\Widgets\Form;
-use Encore\Admin\Widgets\Box;
-use Encore\Admin\Widgets\Collapse;
-use Encore\Admin\Widgets\InfoBox;
-use Encore\Admin\Widgets\Tab;
-use Encore\Admin\Widgets\Table;
-
-// table 1
-$headers = ['Id', 'Email', 'Name', 'Company'];
-$rows = [
-    [1, 'labore21@yahoo.com', 'Ms. Clotilde Gibson', 'Goodwin-Watsica'],
-    [2, 'omnis.in@hotmail.com', 'Allie Kuhic', 'Murphy, Koepp and Morar'],
-    [3, 'quia65@hotmail.com', 'Prof. Drew Heller', 'Kihn LLC'],
-    [4, 'xet@yahoo.com', 'William Koss', 'Becker-Raynor'],
-    [5, 'ipsa.aut@gmail.com', 'Ms. Antonietta Kozey Jr.'],
-];
-
-$table = new Table($headers, $rows);
-
-echo $table->render();
-
-// table 2
-$headers = ['Keys', 'Values'];
-$rows = [
-    'name'   => 'Joe',
-    'age'    => 25,
-    'gender' => 'Male',
-    'birth'  => '1989-12-05',
-];
-
-$table = new Table($headers, $rows);
-
-echo $table->render();
 
 
 
 
+echo Form::textarea('markdown','markdown')->rows(10);
 
-
-$tab = new Tab();
-
-$tab->add('Pie', '2222');
-$tab->add('Table', new Table());
-$tab->add('Text', 'blablablabla....');
-
-echo $tab->render();
-
-
-
-
-$infoBox = new InfoBox('New Users', 'users', 'aqua', '/admin/users', '1024');
-
-echo $infoBox->render();
-
-
-
-
-$collapse = new Collapse();
-
-$collapse->add('Bar', 'xxxxx');
-$collapse->add('Orders', new Table());
-
-echo $collapse->render();
-
-
-//$form = new Form();
-//$form->text('name', '输入框');
-//echo $form->render();
-
-$box = new Box('Box标题', 'Box内容');
-
-$box->removable();
-
-$box->collapsable();
-
-$box->style('info');
-
-$box->solid();
-
-echo $box;
 
 ?>
 
