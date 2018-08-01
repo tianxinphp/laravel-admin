8ce50d8f85dbe8ddc717608fde1fd02b37a184ad
田鑫 <844577216@qq.com>
2018-08-01 21:16:10 +0800

    markdown

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -4,10 +4,13 @@ use Encore\Admin\Widgets\Form;
 
 
 $Form=new Form;
-echo $Form->textarea('editormd_id','markdown')->rows(10)->setWidth(12,0);
+//echo $Form->textarea('editormd_id','markdown')->rows(10)->setWidth(12,0);
 
 
 ?>
+<div id="editormd_id">
+    <textarea style="display:none;">### Hello Editor.md !</textarea>
+</div>
 
 
 
