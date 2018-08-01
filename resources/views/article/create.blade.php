780a2faed09449891d2208b34299bb82fe32eb3d
tianxin <844577216@qq.com>
2018-08-01 14:59:09 +0800

    markdown拓展服务提供者

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -4,7 +4,7 @@ use Encore\Admin\Widgets\Form;
 
 
 $Form=new Form;
-print_r($Form->textarea('markdown','markdown')->rows(10)->getViewElementClasses());
+$Form->textarea('markdown','markdown')->rows(10);
 
 
 ?>
