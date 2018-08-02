b3d91f3789f09cdc7499d90b94abe5891665d046
tianxin <844577216@qq.com>
2018-08-02 14:47:34 +0800

    markdown拓展服务提供者

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -4,7 +4,7 @@ use Encore\Admin\Widgets\Form;
 
 $Form=new Form;
 //echo $Form->textarea('editormd_id','markdown')->rows(10)->setWidth(12,0);
-echo $Form->html('你的html内容',  'editormd_id');
+echo $Form->html('你的html内容',  ['label' => 'editormd_id', 'field' => '', 'form-group' => '']);
 
 ?>
 {{--<div id="editormd_id">--}}
