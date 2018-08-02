ef700c572d46145fd41b554ef841e6714c8f7a75
tianxin <844577216@qq.com>
2018-08-02 13:58:53 +0800

    markdown拓展服务提供者

--- public/vendor/editormd/js/markdown.js
+++ public/vendor/editormd/js/markdown.js
@@ -7,7 +7,7 @@ $(function () {
     Editormd = editormd({
         id: "editormd_id",
         path:window.location.protocol+'//'+window.location.host+'/vendor/editormd/lib/',
-        height:'700',
+        height:'100%',
         width: '100%'
     });
 });
